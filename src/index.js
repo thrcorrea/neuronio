01cde35025d2e1f8768beca96967143147e9db9b
Thales Correa <thales.rcorrea@gmail.com>
2017-09-03 19:20:50 -0300

    :sparkles: Add basic packages, and .env file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
+const winston = require('winston');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT;
 
 const app = express();
 
-app.router('/status', (req, res) => {
-    res.send('ok');
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/status', (req, res) => {
+  res.send('ok');
 });
 
-
-app.listen(3000, () => {
-    console.log(`Servidor ouvindo na porta 3000`);
-})
+app.listen(port, () => {
+  winston.info(`Servidor ouvindo na porta ${port}`);
+});
